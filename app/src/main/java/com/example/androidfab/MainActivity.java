a96294e2009e0d89768387d9a887a63f1d13c780
johnatan.asantos <johnatan1999accourt@gmail.com>
2019-08-19 16:07:32 -0300

    Initial commit

---
+++ app/src/main/java/com/example/androidfab/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.androidfab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton btnFab;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFab = (FloatingActionButton) findViewById(R.id.FABteste);
+
+        btnFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Gerenciar_Activity.class));
+
+            }
+        });
+    }
+}
